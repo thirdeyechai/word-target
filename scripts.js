17115c1e8c63f9bb808c7e64a7853ba3b08af69c
Russell Knight <thirdeyechai@gmail.com>
2020-12-29 10:34:21 +1100

    Added title, guess input box and button

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
+/** Given a word, randomly jumbles the letters */
 function shuffleWord(word) {
     let charArr = word.split("");
     let len = word.length;
 
     for (let i=0; i<len; i++) {
         let j = Math.floor(Math.random() *len);
-
         let temp = charArr[i];
         charArr[i] = charArr[j];
         charArr[j] = temp;
     }
     return charArr;
 }
-
+/** Populates the grid with a word */
 function fillTarget(word) {
     let gridCells = document.getElementById("grid-container").children;
     for (let i=0; i < word.length; i++) {
